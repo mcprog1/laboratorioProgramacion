a7260e9cdd89e4fbd60dea3d81a61c1955d0f383
Ju0w1 <pablopeculio@cras-dev.com>
2021-07-21 12:01:54 -0300

    Comienzo de menu en main

--- LaboratorioProgramacion.cpp
+++ LaboratorioProgramacion.cpp
@@ -20,13 +20,22 @@ int main()
 		switch (opcion) {
 
 		case 1:
-			
+			system("cls");
+			cout << "1. Alta de usuario" << endl;
+			cout << "2. Alta de asignatura" << endl;
+			cout << "3. Asignar docente a asignatura" << endl;
+			cout << "4. Volver" << endl;
 			break;
 		case 2:
-			
+			system("cls");
+			cout << "1. Inicio de clase" << endl;
+			cout << "2. Finalizar clase" << endl;
+			cout << "3. Volver" << endl;
 			break;
 		case 3:
-			
+			system("cls");
+			cout << "1. Inscripcion a asingatura" << endl;
+			cout << "2. Volver" << endl;
 			break;
 		case 4:
 			
